df35911deb385b5ae0142b53f2ff3d62bbaf7b68
Eve Ravignot <eve.ravignot@stuart.be>
2020-10-19 21:33:44 +0200

    Initial commit

---
+++ src/main/java/be/thomasmore/graduaten/gameplay/ServletInitializer.java
@@ -0,0 +1,13 @@
+package be.thomasmore.graduaten.gameplay;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GameplayApplication.class);
+    }
+
+}
---
+++ src/test/java/be/thomasmore/graduaten/gameplay/GameplayApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.graduaten.gameplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
