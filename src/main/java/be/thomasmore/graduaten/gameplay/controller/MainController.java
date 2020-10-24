b0406e683172b7c1fbdc45386d0befd05c442ebe
CelenM <maartencelen@gmail.com>
2020-10-24 11:04:20 +0200

    index.jsp aangemaakt en raadpleegbaar

---
+++ src/main/java/be/thomasmore/graduaten/gameplay/controller/MainController.java
@@ -0,0 +1,21 @@
+package be.thomasmore.graduaten.gameplay.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
