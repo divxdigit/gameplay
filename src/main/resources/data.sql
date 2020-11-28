f032a396123d8b9ae132be3301d67597c457b5ac
CelenM <maartencelen@gmail.com>
2020-11-28 13:07:44 +0100

    database aanpassingen

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,28 @@
-/*SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE if exists OrderProducts;
-DROP TABLE if exists Products;
-DROP TABLE if exists Orders;
-DROP TABLE if exists Users;
-DROP TABLE if exists UserTypes;
-DROP TABLE if exists Publishers;
-DROP TABLE if exists Genres;
-DROP TABLE if exists AgeCategories;
-DROP TABLE if exists Languages;
-SET FOREIGN_KEY_CHECKS = 1;*/
+SET FOREIGN_KEY_CHECKS = 0;
+-- DROP TABLE if exists OrderProducts;
+-- DROP TABLE if exists Products;
+-- DROP TABLE if exists Languages;
+-- DROP TABLE if exists Orders;
+-- DROP TABLE if exists Users;
+-- DROP TABLE if exists UserTypes;
+--
+-- DROP TABLE if exists Publishers;
+-- DROP TABLE if exists Genres;
+-- DROP TABLE if exists AgeCategories;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+Alter table Products
+alter column description longtext;
+
+Alter table Orders
+ADD  delivery_street text;
+Alter table Orders
+ADD  delivery_number text;
+Alter table Orders
+ADD  delivery_postalcode text;
+Alter table Orders
+ADD  delivery_city text;
 
 CREATE TABLE IF NOT EXISTS Publishers(
      id int not null AUTO_INCREMENT,
@@ -107,7 +121,7 @@ values ('Erotisch');
 CREATE TABLE IF NOT EXISTS Products(
    id int not null AUTO_INCREMENT,
    name varchar(50) not null,
-   description varchar(MAX),
+   description longtext,
    genre_id int not null,
    age_category_id int not null,
    players_minimum int,
@@ -129,21 +143,21 @@ CREATE TABLE IF NOT EXISTS Products(
    foreign key (language_id) references Languages (id)
 );
 
-insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
-values ('De Kwakzalvers van Kakelenburg','De Kwakzalvers',1,9,2,4,4,1,9,5,'/images/GameImages/Kwakzalvers-van-Kakelenburg.png',1.89,39.95,2,'2001-01-31',1);
-insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
-values ('Wingspan','Een vogelspel',4,9,1,5,1,1,0,5,'/images/GameImages/3d-wingspan.png',3.50,56.14,1,'2000-06-20',1);
-insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
-values ('Ticket to Ride Europe 2','Met het Ticket',1,9,2,5,3,2,15,3,'/images/GameImages/TicketToRideEurope.jpg',2.45,34.95,1,'2008-02-20',1);
+-- insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
+-- values ('De Kwakzalvers van Kakelenburg','De Kwakzalvers',1,9,2,4,4,1,9,5,'/images/GameImages/Kwakzalvers-van-Kakelenburg.png',1.89,39.95,2,'2001-01-31',1);
+-- insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
+-- values ('Wingspan','Een vogelspel',4,9,1,5,1,1,0,5,'/images/GameImages/3d-wingspan.png',3.50,56.14,1,'2000-06-20',1);
+-- insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
+-- values ('Ticket to Ride Europe 2','Met het Ticket',1,9,2,5,3,2,15,3,'/images/GameImages/TicketToRideEurope.jpg',2.45,34.95,1,'2008-02-20',1);
 
-/*insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
+insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
 values ('De Kwakzalvers van Kakelenburg','De Kwakzalvers van Kakelenburg Eenmaal per jaar komen in Kakelenburg de beste wonderdoktoren en beunhazen samen om drankjes tegen zweetvoeten, heimwee, de hik en liefdesverdriet te brouwen. Iedere kwakzalver kookt hier echter zijn eigen soep. Iedere speler trekt zolang ingrediënten uit zijn eigen, in de loop van het spel zelf samengestelde, voorraadbuidel totdat hij denkt dat zijn drank perfect is. Maar pas op: een paar knalerwten en de hele drank ontploft. Het is dus beter om op tijd te stoppen en een kleinere portie te koken om zo met de opbrengst ervan kostbare ingrediënten aan te schaffen. Zo vult de voorraadbuidel zich met steeds meer nuttige ingrediënten, waardoor de volgende drank wellicht nog beter wordt en meer punten oplevert. In dit spel met meer dan 2000 combinatiemogelijkheden wordt het beproefde deckbuildingssysteem gecombineerd met het populaire “push your luck”-mechanisme.',1,9,2,4,4,1,9,5,'/images/GameImages/Kwakzalvers-van-Kakelenburg.png',1.89,39.95,2,'2001-01-31',1);
 insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
 values ('Wingspan','Een vogelspel vol tactiek en spanning Als vogelliefhebber probeer je de beste leefomstandigheden voor de vogels in je opvangcentrum te creëren. Elke vogel breidt in één van zijn leefgebieden een ketting van krachtige combinaties uit. Deze leefgebieden richten zich op verschillende groeifactoren: voedsel pakken, eieren leggen en nieuwe vogelkaarten trekken. Wie bouwt het beste actiemotortje en scoort aan het einde van het spel de meeste punten met zijn vogels? Elke beurt speel je een vogel, waarna je alle acties die zich in het betreffende leefgebied bevinden, mag uitvoeren. Zo kun je voedsel uit een “echt” vogelhuisje pakken, ei-miniaturen in nesten leggen of nieuwe vogels verzamelen. Alle opties leveren op verschillende manieren punten op. Het spel bevat 170 unieke vogelkaarten, die in combinatie met de verschillende bonuskaarten en de interactie met de andere spelers voor oneindige variatie zorgen. Het spel is ook solitair te spelen!',4,9,1,5,1,1,0,5,'/images/GameImages/3d-wingspan.png',3.50,56.14,1,'2000-06-20',1);
 insert into Products(name,description,genre_id,age_category_id,players_minimum,players_maximum,rating,rent_stock,buy_stock,publisher_id,picture,rent_price,buy_price,language_id,date_launch,active)
 values ('Ticket to Ride Europe 2','Met het Ticket to Ride Europe bordspel reis je per trein over het hele continent. Waag je een reis door de donkere tunnels in Zwitserland of aan boord van een veerboot op de Zwarte Zee? Of bouw je schitterende treinstations in de grote hoofdsteden van oude wereldrijken?
 In Ticket to Ride Europe moeten de spelers de routes op hun bestemmingskaarten volbrengen. Dit doe je door verbindingen tussen steden te claimen, die je mag bouwen als je de juiste kaartencombinaties in handen hebt. Heb jij aan het eind van het spel de meeste punten verzameld met je treinnetwerk? Dan ben jij de winnaar.
-Dit populaire bordspel is een makkelijk aan te leren spel met diverse onvoorziene plotwendingen, zodat de uiteindelijke winnaar tot op het laatst onbekend blijft. Spanning en plezier voor het hele gezin.',1,9,2,5,3,2,15,3,'/images/GameImages/TicketToRideEurope.jpg',2.45,34.95,1,'2008-02-20',1);*/
+Dit populaire bordspel is een makkelijk aan te leren spel met diverse onvoorziene plotwendingen, zodat de uiteindelijke winnaar tot op het laatst onbekend blijft. Spanning en plezier voor het hele gezin.',1,9,2,5,3,2,15,3,'/images/GameImages/TicketToRideEurope.jpg',2.45,34.95,1,'2008-02-20',1);
 
 CREATE TABLE IF NOT EXISTS Usertypes(
     id int not null AUTO_INCREMENT,
@@ -190,16 +204,27 @@ CREATE TABLE IF NOT EXISTS Orders(
      date_created date,
      date_collect date,
      status int,
+     delivery_street text,
+     delivery_number text,
+     delivery_postalcode text,
+     delivery_city text,
      foreign key (user_id) references Users (id),
      primary key (id)
 );
 
-insert into Orders(user_id, date_created, date_collect, status)
-VALUES (1,'2020-10-10','2020-10-11',1);
-insert into Orders(user_id, date_created, date_collect, status)
-VALUES (3,'2020-08-10','2020-08-11',1);
-insert into Orders(user_id, date_created, date_collect, status)
-VALUES (1,'2020-09-10','2020-12-11',0);
+insert into Orders(user_id, date_created, date_collect, status, delivery_street, delivery_number, delivery_postalcode, delivery_city)
+VALUES (1,'2020-10-10','2020-10-11',1,'Kerkstraat','7A','2230','Herselt');
+insert into Orders(user_id, date_created, date_collect, status, delivery_street, delivery_number, delivery_postalcode, delivery_city)
+VALUES (3,'2020-08-10','2020-08-11',1,'Teststraat','18','2440','Geel');
+insert into Orders(user_id, date_created, date_collect, status, delivery_street, delivery_number, delivery_postalcode, delivery_city)
+VALUES (1,'2020-09-10','2020-12-11',0,'Dorpsplein','24','2230','Herselt');
+
+-- insert into Orders(user_id, date_created, date_collect, status)
+-- VALUES (1,'2020-10-10','2020-10-11',1);
+-- insert into Orders(user_id, date_created, date_collect, status)
+-- VALUES (3,'2020-08-10','2020-08-11',1);
+-- insert into Orders(user_id, date_created, date_collect, status)
+-- VALUES (1,'2020-09-10','2020-12-11',0);
 
 
 CREATE TABLE IF NOT EXISTS Order_Products(
